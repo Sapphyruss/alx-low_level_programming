9e12ac20ab433014ea5bef48df21278d9f427f0b
Sapphyruss <lugi.franklin@outlook.fr>
2023-07-06 17:45:34 -0700

    .

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,25 +1,25 @@
-#include "main.h"
+#include <main.h>
 
-/**
- * binary_to_uint - convert a binary number to an int
- * @b: binary number
- * Return: int number
- */
-
-unsigned int binary_to_uint(const char *b)
-{
-	unsigned int num = 0;
-	int i, base = 1, n;
-
-	if (b == NULL)
-		return (0);
-	n = (int)strlen(b);
-	for (i = (n - 1); i >= 0; i--)
-	{
-		if (b[i] != '0' && b[i] != '1')
-			return (0);
-		num += (b[i] - '0') * base;
-		base *= 2;
-	}
-	return (num);
+unsigned int binary_to_uint(const char *b) {
+    // Return 0 if b is NULL
+    if (b == NULL) {
+        return 0;
+    }
+    
+    unsigned int result = 0;
+    int len = strlen(b);
+    
+    // Iterate over each character in the string
+    for (int i = 0; i < len; i++) {
+        // If a non-binary character is found, return 0
+        if (b[i] != '0' && b[i] != '1') {
+            return 0;
+        }
+        
+        // Convert the binary digit to integer and update the result
+        result = result * 2 + (b[i] - '0');
+    }
+    
+    return result;
 }
+
