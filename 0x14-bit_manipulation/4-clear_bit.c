f0ca1c46c8d377a777c1670e53d605ec69fbfc18
Sapphyruss <lugi.franklin@outlook.fr>
2023-07-06 17:35:16 -0700

    .

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+include "main.h"
+
+/**
+ * clear_bit - change the bit to 0 at given index
+ * @n: pointer to the number
+ * @index: index to change the bit at
+ * Return: 1 success and -1 if fail
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(index) * 8)
+	{
+		return (-1);
+	}
+	*n &= ~(1 << index);
+	return (1);
+}
