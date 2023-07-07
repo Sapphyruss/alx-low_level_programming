7244cd338d8b5348e08fc15a6229b78dba74f248
Sapphyruss <lugi.franklin@outlook.fr>
2023-07-06 17:56:05 -0700

    .

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,25 +1,18 @@
 #include <main.h>
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int decimal;
+	unsigned int i;
 
-unsigned int binary_to_uint(const char *b) {
-    // Return 0 if b is NULL
-    if (b == NULL) {
-        return 0;
-    }
-    
-    unsigned int result = 0;
-    int len = strlen(b);
-    
-    // Iterate over each character in the string
-    for (int i = 0; i < len; i++) {
-        // If a non-binary character is found, return 0
-        if (b[i] != '0' && b[i] != '1') {
-            return 0;
-        }
-        
-        // Convert the binary digit to integer and update the result
-        result = result * 2 + (b[i] - '0');
-    }
-    
-    return result;
+	for (decimal = 0, i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] == '1')
+			decimal = (decimal << 1) | 1;
+		else if (b[i] == '0')
+			decimal <<= 1;
+		else if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+
+	return (decimal);
 }
-
